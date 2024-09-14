5dbaba214ed89c3486fd55771f2fa3d74f6060ea
Caio Oliveira <caioaao@gmail.com>
2024-09-14 11:58:06 -0300

    (nvim) command to create branch from head

--- modules/nvim/.config/nvim/init.lua
+++ modules/nvim/.config/nvim/init.lua
@@ -235,6 +235,7 @@ require("lazy").setup({
 	{
 		"tpope/vim-fugitive",
 		config = function()
+			vim.api.nvim_create_user_command("Gc", "G checkout -b <args> HEAD", { nargs = 1 })
 			vim.api.nvim_create_user_command("Gco", "G checkout -b <args> origin", { nargs = 1 })
 			vim.api.nvim_create_user_command("Gpo", "G push -u origin HEAD <args>", { nargs = "*" })
 			vim.api.nvim_create_user_command("Gpof", "G push -u origin HEAD --force-with-lease", {})
