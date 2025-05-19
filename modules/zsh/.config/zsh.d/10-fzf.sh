da01337f772f4b95cc5153e5119013b57eaa0202
Caio Oliveira <caioaao@gmail.com>
2025-05-19 08:31:11 -0300

    catppuccin everywhere

--- modules/zsh/.config/zsh.d/10-fzf.sh
+++ modules/zsh/.config/zsh.d/10-fzf.sh
@@ -1,1 +1,9 @@
 [ -f "$HOME/.config/fzf/fzf.zsh" ] && source "$HOME/.config/fzf/fzf.zsh"
+
+# from https://raw.githubusercontent.com/catppuccin/fzf/refs/heads/main/themes/catppuccin-fzf-latte.sh
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
+--color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
+--color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
+--color=selected-bg:#BCC0CC \
+--color=border:#CCD0DA,label:#4C4F69"
