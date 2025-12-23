b9c143465c24bdc799bf6e11c6af30d9dfd947c6
Caio Oliveira <caioaao@gmail.com>
2025-12-23 10:17:48 -0300

    Git spice aliases

--- stow-modules/git_spice/.config/zsh.d/10-git-spice.sh
+++ stow-modules/git_spice/.config/zsh.d/10-git-spice.sh
@@ -2,3 +2,36 @@
 autoload -U +X compinit && compinit
 
 eval "$(gs shell completion zsh)"
+
+# Git-spice aliases for common commands
+
+# Repository operations
+alias gss='gs repo sync'
+
+# Branch navigation
+alias gsu='gs up'
+alias gsd='gs down'
+alias gst='gs top'
+alias gsb='gs bottom'
+alias gstrunk='gs trunk'
+
+# Branch operations
+alias gsco='gs b checkout'
+alias gsc='gs b create'
+gscm() { gs b create --target main "$@"; }
+alias gsub='gs b submit'
+alias gsr='gs b restack'
+alias gsrm='gs b delete'
+alias gsmv='gs b rename'
+
+# Stack operations
+alias gssr='gs s restack'
+alias gsse='gs s edit'
+
+# Rebase operations
+alias gsrc='gs rebase continue'
+alias gsra='gs rebase abort'
+
+# Log operations
+alias gsl='gs log short'
+alias gsll='gs log long'
