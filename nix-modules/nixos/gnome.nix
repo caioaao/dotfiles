38adaac5b5a28ec6aa3ea02d58a3e7654a797772
Caio <caioaao@gmail.com>
2026-02-03 21:21:06 -0300

    Fix build warnings

--- nix-modules/nixos/gnome.nix
+++ nix-modules/nixos/gnome.nix
@@ -4,8 +4,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Turn on the dconf module (lets us set GNOME/GSettings system-wide)
   programs.dconf = {
