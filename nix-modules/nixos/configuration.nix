207c4e4a118e94044e3b7deae70781109007d467
Caio <caioaao@gmail.com>
2026-02-17 12:33:07 -0300

    nix-ld.enable = true

--- nix-modules/nixos/configuration.nix
+++ nix-modules/nixos/configuration.nix
@@ -131,6 +131,8 @@
   # PCSC-lite daemon, to access smart cards (Yubikey)
   services.pcscd.enable = true;
 
+  programs.nix-ld.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
