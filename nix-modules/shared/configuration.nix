8949049d1a1dbbee3f422e196139526a7de45082
Caio <caioaao@gmail.com>
2026-03-05 20:59:59 -0300

    Add rtk

--- nix-modules/shared/configuration.nix
+++ nix-modules/shared/configuration.nix
@@ -13,6 +13,32 @@ let
     vendorHash = "sha256-T6zSwQdDWYQqe8trIlhpU8dUQXtz8OGmnW5L5AVjGn8=";
     subPackages = [ "." ];
   };
+
+  rtk = pkgs.stdenv.mkDerivation rec {
+    pname = "rtk";
+    version = "0.27.0";
+    src = let
+      sources = {
+        x86_64-linux = pkgs.fetchurl {
+          url = "https://github.com/rtk-ai/rtk/releases/download/v${version}/rtk-x86_64-unknown-linux-musl.tar.gz";
+          hash = "sha256-3Y425o0+1z2/fvJPlRdJibU7bUL8OvBcgwt6stJgb2s=";
+        };
+        aarch64-darwin = pkgs.fetchurl {
+          url = "https://github.com/rtk-ai/rtk/releases/download/v${version}/rtk-aarch64-apple-darwin.tar.gz";
+          hash = "sha256-A1zQBRJgsEUuRXbletRV+LFsLgtaZ8AFifPcB5f0ti4=";
+        };
+      };
+    in sources.${pkgs.stdenv.hostPlatform.system}
+      or (throw "rtk: unsupported system ${pkgs.stdenv.hostPlatform.system}");
+    sourceRoot = ".";
+    dontConfigure = true;
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/bin
+      cp rtk $out/bin/rtk
+      chmod +x $out/bin/rtk
+    '';
+  };
 in {
   imports = [
     ./tmux/default.nix
@@ -47,6 +73,7 @@ in {
     obsidian
     oh-my-posh
     ripgrep
+    rtk
     stdenv
     stow
     unzip
