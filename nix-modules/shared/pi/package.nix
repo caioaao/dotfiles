2abf00da1431ff3d1f1112ecb5e1efaddeeab63a
Caio Oliveira <caio@liveflow.io>
2026-09-07 09:34:35 -0300

    Bump pi

--- nix-modules/shared/pi/package.nix
+++ nix-modules/shared/pi/package.nix
@@ -15,10 +15,10 @@
 # The CLI binary is still `pi`; only the scope/owner changed.
 
 let
-  version = "0.84.2";
+  version = "0.85.1";
   tarball = fetchurl {
     url = "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-${version}.tgz";
-    hash = "sha256-lbiZzXsaDB8BdMe/M6tCdDXjVTp9H0dWZhqpx/Gmj/o=";
+    hash = "sha256-H0mHKWSb3OZH0RYJk7TZK/PGFMyBkhO+4vkd008qevQ=";
   };
   srcWithLock = runCommand "pi-${version}-src" {} ''
     mkdir -p $out
@@ -31,7 +31,7 @@ buildNpmPackage {
   pname = "pi";
   inherit version;
   src = srcWithLock;
-  npmDepsHash = "sha256-Nhsx3cW58JkGnfPa/LvUWVtpfLALq5vbvxBczcBPUMc=";
+  npmDepsHash = "sha256-6JUjmWyCogZGgg2qvmpHPSO+mp4eoYWaKtOJ/08YJa8=";
   makeCacheWritable = true;
   dontNpmBuild = true;
   npmFlags = [ "--legacy-peer-deps" ];
